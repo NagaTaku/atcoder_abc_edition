1a74d0f721abd767572c85f2fd8f1bf9faca0fcf
永瀬拓也 <nagasetakuya@nagasetakuyanoMacBook-Air-2.local>
2020-04-29 04:11:12 +0900

    c is compleated

--- ABC059/c.py
+++ ABC059/c.py
@@ -2,39 +2,42 @@ n = int(input())
 
 a = list(map(int, input().split()))
 
-total = a[0]
-ans = 0
-hugo = 0
-if total == 0:
-    if a[1] >= 0:
-        total = -1
-        ans = 1
-        hugo = -1
-    else:
-        total = 1
-        ans = 1
-        hugo = 1
-elif total > 0:
-    ans = 0
-    hugo = 1
-elif total < 0:
-    ans = 0
-    hugo = -1
+total = 0
+ans_m = 0
+ans_p = 0
 
-
-for i in range(1,n):
+hugo = 1
+for i in range(n):
     total += a[i]
     if hugo == -1 and total <= 0:
-        ans = ans + (1-total)
+        ans_p = ans_p + (1-total)
         total = 1
         hugo = 1
     elif hugo == -1 and total > 0:
         hugo = 1
     elif hugo == 1 and total >= 0:
-        ans = ans + (1+total)
+        ans_p = ans_p + (1+total)
         total = -1
         hugo = -1
     elif hugo == 1 and total < 0:
         hugo = -1
 
-print(ans)
+total = 0
+hugo = -1
+
+for i in range(n):
+    total += a[i]
+    if hugo == -1 and total <= 0:
+        ans_m = ans_m + (1-total)
+        total = 1
+        hugo = 1
+    elif hugo == -1 and total > 0:
+        hugo = 1
+    elif hugo == 1 and total >= 0:
+        ans_m = ans_m + (1+total)
+        total = -1
+        hugo = -1
+    elif hugo == 1 and total < 0:
+        hugo = -1
+
+print(min(ans_m, ans_p))
