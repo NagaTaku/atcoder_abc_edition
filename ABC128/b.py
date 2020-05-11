70aeba3e2ebfe5dbcad8a15df093c883e50ab29c
永瀬拓也 <nagasetakuya@nagasetakuyanoMacBook-Air-2.local>
2020-05-11 22:21:43 +0900

    b is comp

--- ABC128/b.py
+++ ABC128/b.py
@@ -5,9 +5,16 @@ score = {}
 for i in range(n):
     s, p = input().split()
 
-    if s in score.keys:
-        score[s].appenc(int(p))
-    else:
-        score[s] = list(int(p))
+    if s not in score:
+        score[s] = []
 
-print(score)
+    t = [i+1,int(p)]
+    score[s].append(t)
+
+city = sorted(score)
+for key in city:
+    score[key] = sorted(score[key], key=lambda x: x[1], reverse=True)
+
+    for i in range(len(score[key])):
+        print(score[key][i][0])
+    
