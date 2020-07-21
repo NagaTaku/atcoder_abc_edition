980e15f803dddf10ce5b839e7cdfad97f36c2a48
NagaTaku <greeeen0315@gmail.com>
2020-07-21 21:32:09 +0900

    d is done

--- ABC168/d2.py
+++ ABC168/d2.py
@@ -1,12 +1,25 @@
-import numpy as np
-from scipy.sparse.csgraph import shortest_path, floyd_warshall, dijkstra, bellman_ford, johnson
-from scipy.sparse import csr_matrix
-
 N, M = map(int, input().split())
+route = [[] for _ in range(N)]
+sign = [0]*N
 
-route_list = np.array([[0 for i in range(N)] for j in range(N)])
-
+#print(route)
 for i in range(M):
     a,b = map(int, input().split())
-    route_list[a-1][b-1] = 1
-    route_list[b-1][a-1] = 1
+    route[a-1].append(b-1)
+    route[b-1].append(a-1)
+#print(route)
+
+marked = {0}
+q = [0]
+for i in q:
+    for j in route[i]:
+        if j in marked:
+            continue
+        q.append(j)
+        marked.add(j)
+        sign[j] = i+1
+
+print('Yes')
+[print(i) for i in sign[1:]]
+
+
