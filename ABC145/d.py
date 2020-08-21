9a959eb75aeca6b75fa48e706b9f2b6b12d867de
NagaTaku <greeeen0315@gmail.com>
2020-08-21 17:02:14 +0900

    D問題解いた

--- ABC145/d.py
+++ ABC145/d.py
@@ -1,22 +1,21 @@
-import math
 fact = [None]*1000001
+inv = [0,1]
+finv = [1,1]
 
 x, y = map(int, input().split())
 total = x+y
 ans = 0
 MOD = 10**9+7
 
-fact[1] = 1
-for i in range(2,1000001):
-    fact[i] = fact[i-1]*i
-
-print('fin')
 if total%3 == 0 and max(x,y) <= min(x,y)*2:
+    fact[1] = 1
+    for i in range(2,1000001):
+        fact[i] = fact[i-1]*i%MOD
+        inv.append((-inv[MOD%i] * (MOD//i)) % MOD)
+        finv.append(finv[-1] * inv[-1] % MOD)
     step = int(total/3)
     sa = abs(x-y)
     a = int((step-sa)/2)
     b = int((step+sa)/2)
-    ans = fact[step] / (fact[a]*fact[b])
-
-ans %= MOD
-print(int(ans))
+    ans = fact[step] * finv[a] * finv[b] % MOD
+print(ans)
